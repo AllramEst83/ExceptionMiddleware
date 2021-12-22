800dfa995d3099ba187236f719c692000c0f8846
Kay Wiberg <kaywib@gmail.com>
2021-12-22 10:12:02 +0100

    Create ExceptionMiddleware.cs

---
+++ ExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using BestSolution.Models;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BestSolution.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "The application has encountered an unknown error.");
+
+                await HandleExceptionAsync(httpContext);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        
+            JsonSerializerSettings camelSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            string result = JsonConvert.SerializeObject(new Result().Fail("The request could not be completed."), camelSettings);
+
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
